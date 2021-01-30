6137b32709d8031dd314580b3d05d34eb55dd065
Margaret <rgboiko@gmail.com>
2021-01-30 21:14:08 +0200

    Fix

---
+++ gulpfile.js
@@ -0,0 +1,60 @@
+let project_folder= 'distin';
+let source_folder = 'src';
+
+let path = {
+	build:{
+		html: project_folder+'/page/',
+		css: project_folder+'/css/',
+		js: project_folder+'/js/',
+		img: project_folder+'/img/',
+		fonts: project_folder+'/fonts/',
+	},
+	src:{
+		html: [source_folder+'/page/*.html', '!' + source_folder+'/page/*.html'],
+		css: source_folder+'/scss/style.scss',
+		js: source_folder+'/js/script.js',
+		img: source_folder+'/img/**/*.{jpg,png,svg,gif,webp}',
+		fonts: source_folder+'/fonts/*.ttf',
+	},
+		watch:{
+		html: source_folder+'/**/*.html',
+		css: source_folder+'/scss/**/*.scss',
+		js: source_folder+'/js/**/*.js',
+		img: source_folder+'/img/**/*.{jpg,png,svg,gif,webp}',
+	},
+	clean: './'+project_folder+'/'
+}
+
+let { src, dest } = require('gulp'),
+	gulp = require('gulp'),
+	browsersync = require('browser-sync').create(),
+	fileinclude = require('gulp-file-include');
+	
+function browserSync(params){
+	browsersync.init({
+		server:{
+			baseDir: "./" +project_folder + "/"
+		},
+		port: 3000, 
+		notify: false
+	})
+}
+
+function html(){
+	return src(path.src.html)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream())
+}
+
+function watchFiles (params){
+	gulp.watch([path.watch.html], html);
+}
+
+let build = gulp.series(html); 
+let watch = gulp.parallel(build, watchFiles, browserSync);
+
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
